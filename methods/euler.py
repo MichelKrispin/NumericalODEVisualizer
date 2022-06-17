714a51f78abedfb6b9d89af432c45041c6b816c8
MichelKrispin <michel.krispin@gmail.com>
2022-06-17 16:41:50 +0200

    Implmented explicit euler

--- methods/euler.py
+++ methods/euler.py
@@ -1,16 +1,30 @@
-def method(f, y, options):
-    """Very interesting function
+import numpy as np
+
+
+def method(f, y0, t0, te, options=None):
+    """Simple explicit euler method from [t0, te] with y(t0) = y0 and f(t, y) = y'.
 
     Args:
-        f (_type_): _description_
-        y (_type_): _description_
-        options (_type_): _description_
+        f (function): f(t, y) = y'(t)
+        y0 (float): Initial value
+        t0 (float): Left boundary
+        te (float): Right boundary
+        options (dict, optional): An optionial options dictionary.
 
     Returns:
-        _type_: _description_
+        list(float): The computed y values.
     """
     print('Calling the euler method')
-    return [0, 1, 2, 3, 4, 5]
+    h = options['data']       # Get the step size from the options
+    t = np.arange(t0, te, h)  # Create the t values
+    y = np.zeros(t.shape)     # Create the resulting y values
+    y[0] = y0                 # Store the initial value
+    for n in range(len(t)-1):
+        y[n+1] = y[n] + f(t[n], y[n]) * h
+    return y
 
 
-options = [{'name': 'First option', 'data': ['arbitrary']}]
+options = [
+    {'name': 'Step Size (h): 0.001', 'data': 0.001},
+    {'name': 'Step Size (h): 0.0001', 'data': 0.0001},
+]
